b2f9cf81cae66763132ba38636fdf7682b54a0c2
Mikirtumov <you@example.com>
2020-06-05 03:01:18 -0700

    Updates

---
+++ precache-manifest.c95995f73d0149cceb938fcca9a75afb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba62671b4da2764b5a3b1b08c13b41d",
+    "url": "/React-Kanban/index.html"
+  },
+  {
+    "revision": "95d5bc47168ed5c0f45a",
+    "url": "/React-Kanban/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a88b6222cc0eb6ed57c4",
+    "url": "/React-Kanban/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95d5bc47168ed5c0f45a",
+    "url": "/React-Kanban/static/js/2.bb199d4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Kanban/static/js/2.bb199d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88b6222cc0eb6ed57c4",
+    "url": "/React-Kanban/static/js/main.02f20348.chunk.js"
+  },
+  {
+    "revision": "58d431a5375df389a49b",
+    "url": "/React-Kanban/static/js/runtime-main.6f788490.js"
+  }
+]);
